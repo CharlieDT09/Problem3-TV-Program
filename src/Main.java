5b129b7508db9d6600ee3bdc31286b6a607d1048
Charlie David <davys4802@gmail.com>
2026-09-02 16:02:52 -0500

    Entire Program

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        try
+        {
+            System.out.print("Ingrese la cantidad de episodios: ");
+            int episodios = Integer.parseInt(br.readLine());
+            System.out.print("Ingrese la cantidad de días: ");
+            int dias = Integer.parseInt(br.readLine());
+            int resultado = episodios / dias;
+            System.out.println("Episodios por día: " + resultado);
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        catch (ArithmeticException e) {
+            System.out.println("Error: no se puede dividir entre cero.");
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Error: debe ingresar números enteros.");
+        }
+        catch (Exception e) {
+            System.out.println("Error general.");
         }
     }
 }
--- src/ProgramaDeTV.java
+++ src/ProgramaDeTV.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+public class ProgramaDeTV
+{
+    private int episodios;
+    private int dias;
 
-public class ProgramaDeTV {
+    public int getEpisodios()
+    {
+        return episodios;
+    }
+
+    public void setEpisodios(int episodios)
+    {
+        this.episodios = episodios;
+    }
+
+    public int getDias()
+    {
+        return dias;
+    }
+
+    public void setDias(int dias)
+    {
+        this.dias = dias;
+    }
+
+    public int resultado()
+    {
+        return episodios / dias;
+    }
 }
